b076ee4b03d311f9a74c13ae832bd93d0af15a83
Amanda Dell <amhernandez1163@gmail.com>
2022-04-16 16:09:13 -0500

    updated package.json to remove node

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,7 +1,11 @@
 const Employee = require("../lib/Employee");
-const Employee = new Employee("San Holo", "11261990", "stayvibrant@gmail.com");
 
-test("test for constructor values from Employee", () => {
+test("test for constructor values from Employee to match contst Employee input below", () => {
+  const Employee = new Employee(
+    "San Holo",
+    "11261990",
+    "stayvibrant@gmail.com"
+  );
   expect(employee.name).toBe("San Holo");
   expect(employee.id).toBe("11261990");
   expect(employee.email).toBe("stayvibrant@gmail.com");
