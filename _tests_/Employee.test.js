b4c5dcb3218b66fd61b87c457c20a3804fec4b0b
Amanda Dell <amhernandez1163@gmail.com>
2022-04-16 17:36:30 -0500

    updated Employee test to include functions

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -11,3 +11,43 @@ test("test for constructor values from Employee to match contst Employee input b
   expect(myEmployee.id).toBe("11261990");
   expect(myEmployee.email).toBe("stayvibrant@gmail.com");
 });
+
+test("test to verify if getName shows San Holo", () => {
+  const myEmployee = new Employee(
+    "San Holo",
+    "11261990",
+    "stayvibrant@gmail.com"
+  );
+
+  expect(myEmployee.getName()).toBe("San Holo");
+});
+
+test("test to verify if getId shows 11261990", () => {
+  const myEmployee = new Employee(
+    "San Holo",
+    "11261990",
+    "stayvibrant@gmail.com"
+  );
+
+  expect(myEmployee.getId()).toBe("11261990");
+});
+
+test("test to verify if getEmail shows stayvibrant@gmail.com", () => {
+  const myEmployee = new Employee(
+    "San Holo",
+    "11261990",
+    "stayvibrant@gmail.com"
+  );
+
+  expect(myEmployee.getEmail()).toBe("stayvibrant@gmail.com");
+});
+
+test("test to verify if getRole shows Employee", () => {
+  const myEmployee = new Employee(
+    "San Holo",
+    "11261990",
+    "stayvibrant@gmail.com"
+  );
+
+  expect(myEmployee.getRole()).toBe("Employee");
+});
