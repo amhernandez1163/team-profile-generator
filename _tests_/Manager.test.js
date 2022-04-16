252d30585e00b51750789da6571cdb2818bdd636
Amanda Dell <amhernandez1163@gmail.com>
2022-04-16 17:50:12 -0500

    added function testing to Manager - all passing

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -14,3 +14,25 @@ test("test for constructor values for Manager to match const input below", () =>
   expect(myManager.email).toBe("johnnie@gmail.com");
   expect(myManager.officeNumber).toBe("2105555555");
 });
+
+test("test to verify if getOfficeNumber shows 2105555555", () => {
+  const myManager = new Manager(
+    "Johnnie",
+    "1202",
+    "johnnie@gmail.com",
+    "2105555555"
+  );
+
+  expect(myManager.getOfficeNumber()).toBe("2105555555");
+});
+
+test("test to verify if getRole shows Manager", () => {
+  const myManager = new Manager(
+    "Johnnie",
+    "1202",
+    "johnnie@gmail.com",
+    "2105555555"
+  );
+
+  expect(myManager.getRole()).toBe("Manager");
+});
