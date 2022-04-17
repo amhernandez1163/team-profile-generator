ef2b2e022374d2d4e041f324c3193c4eb56f5929
Amanda Dell <amhernandez1163@gmail.com>
2022-04-17 11:51:11 -0500

    completed HTML and testing

--- src/generateContent.js
+++ src/generateContent.js
@@ -5,8 +5,8 @@ const generateContent = (team) => {
   const createManager = (newManager) => {
     console.log(newManager);
     let managerHTML = `
-        <div class="columns is-variable">
-            <div class="card column box">
+        <div class="card">
+            <div class="card-header box">
                 <h1 class="title"> ${newManager.name}</h1>
                 <h3 class="subtitle"> Manager </h3>
                 <ul>
@@ -14,10 +14,10 @@ const generateContent = (team) => {
                         <p> Manager ID: ${newManager.id}</p>
                     </li>
                     <li>
-                        <p> Primary Email: <a href="mailto:${newManager.email}" target="_blank"> </p>
+                        <p> Primary Email: <a href="mailto:${newManager.email}" target="_blank">${newManager.email}</a></p>
                     </li>
                     <li>
-                        <p> Office Number: ${newManager.officeNumber}/p>
+                        <p> Office Number: ${newManager.officeNumber}</p>
                     </li>
                 </ul>
             </div>
@@ -29,8 +29,8 @@ const generateContent = (team) => {
   const createEngineer = (newEngineer) => {
     console.log(newEngineer);
     let engineerHTML = `
-        <div class="columns is-variable">
-            <div class="card column box">
+        <div class="card">
+            <div class="card-header box">
                 <h1 class="title"> ${newEngineer.name}</h1>
                 <h3 class="subtitle"> Engineer </h3>
                 <ul>
@@ -38,10 +38,10 @@ const generateContent = (team) => {
                         <p> Engineer ID: ${newEngineer.id}</p>
                     </li>
                     <li>
-                        <p> Primary Email: <a href="mailto:${newEngineer.email}" target="_blank"> </p>
+                        <p>Primary Email: <a href="mailto:${newEngineer.email}" target="_blank">${newEngineer.email}</a></p>
                     </li>
                     <li>
-                        <p> GitHub Profile: <a href="https://github.com/${newEngineer.github}" target="_blank"> </p>
+                        <p> GitHub Profile: <a href="https://github.com/${newEngineer.github}" target="_blank">${newEngineer.github}</a></p>
                     </li>
                 </ul>
             </div>
@@ -53,8 +53,8 @@ const generateContent = (team) => {
   const createIntern = (newIntern) => {
     console.log(newIntern);
     let internHTML = `
-        <div class="columns is-variable">
-            <div class="card column box">
+        <div class="card">
+            <div class="card-header box">
                 <h1 class="title"> ${newIntern.name}</h1>
                 <h3 class="subtitle"> Intern </h3>
                 <ul>
@@ -62,7 +62,7 @@ const generateContent = (team) => {
                         <p> Intern ID: ${newIntern.id}</p>
                     </li>
                     <li>
-                        <p> Primary Email: <a href="mailto:${newIntern.email}" target="_blank"> </p>
+                        <p> Primary Email: <a href="mailto:${newIntern.email}" target="_blank">${newIntern.email}</a> </p>
                     </li>
                     <li>
                         <p> School: ${newIntern.school}</p>
@@ -96,24 +96,27 @@ module.exports = (team) => {
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.3/css/bulma.css" integrity="sha512-7VGuxKU1BFMmA+dC7NiW8jF0YOIe6bibjUBr42unVtEsI/UYzXMS3nkgNvmsY4yqauxeiEs4bXF6fPLsCuxN/A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="./assests/css/style.css"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <title>Team Profile Generator</title>
 </head>
 
 <body>
+<main>
 <!--Header-->
   <section class="hero is-small is-primary">
-    <div class="hero-body">
       <p class="title">
         My Team
       </p>
-    </div>
   </section>
-  <main> ${generateContent(team)}</main>
+
+  ${generateContent(team)}
+  
+  </main>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
     `;
 };
-
-module.exports = generateContent;
